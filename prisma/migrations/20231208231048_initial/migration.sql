7201b09f00d2cc9f9f7871cb80efebda676f1432
Vinicius Gomes <pain-nici2011@live.com>
2023-12-08 20:12:43 -0300

    model expert created

---
+++ prisma/migrations/20231208231048_initial/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "experts" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "experts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "experts_email_key" ON "experts"("email");
