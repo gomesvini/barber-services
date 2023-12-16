2a5955a5f56e54cb18cbe9cc6c307d81409b0ec4
Vinicius Gomes <pain-nici2011@live.com>
2023-12-15 21:04:12 -0300

    implemented local strategy - guards

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,20 @@
+import {
+	Controller,
+	HttpStatus,
+	Post,
+	Req,
+	Res,
+	UseGuards
+} from '@nestjs/common'
+import { Request, Response } from 'express'
+import LocalAuthGuard from './guards/local-guard'
+
+@Controller()
+export class AuthController {
+	@UseGuards(LocalAuthGuard)
+	@Post('login')
+	async login(@Req() req: Request, @Res() res: Response) {
+		const { user } = req
+		return res.status(HttpStatus.OK).json(user)
+	}
+}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,11 @@ import { AuthService } from './auth.service'
 import { UsersService } from 'src/users/users.service'
 import { PassportModule } from '@nestjs/passport'
 import { LocalStrategy } from './strategies/local.strategy'
+import { AuthController } from './auth.controller'
 
 @Module({
 	imports: [PassportModule],
-	providers: [AuthService, UsersService, LocalStrategy]
+	providers: [AuthService, UsersService, LocalStrategy],
+	controllers: [AuthController]
 })
 export class AuthModule {}
